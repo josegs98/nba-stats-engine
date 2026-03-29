536feca71c9ec30b757d936c926ae0d3760bdd15
josemanuelgarciasosa <josegsosa98@gmail.com>
2026-03-29 17:06:50 +0200

    Creamos los métodos del cliente tirando contra la api de BallDontLie

--- src/main/java/com/josegs98/nbastats/nba_stats_engine/client/BallDontLieClient.java
+++ src/main/java/com/josegs98/nbastats/nba_stats_engine/client/BallDontLieClient.java
@@ -1,4 +1,41 @@
 package com.josegs98.nbastats.nba_stats_engine.client;
 
+import com.josegs98.nbastats.nba_stats_engine.client.dto.ApiResponseRecord;
+import com.josegs98.nbastats.nba_stats_engine.client.dto.GameStatsRecord;
+import com.josegs98.nbastats.nba_stats_engine.client.dto.PlayerRecord;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
 public class BallDontLieClient {
+
+    private final RestClient restClient;
+
+    public BallDontLieClient(RestClient ballDontLieRestClient){
+        this.restClient = ballDontLieRestClient;
+    }
+
+    public ApiResponseRecord<PlayerRecord> searchPlayers(String search) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/players")
+                        .queryParam("search", search)
+                        .queryParam("per_page", 25)
+                        .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+    }
+
+    public ApiResponseRecord<GameStatsRecord> getPlayerStats(Long playerId, Integer season) {
+        return restClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/stats")
+                        .queryParam("player_ids[]", playerId)
+                        .queryParam("seasons[]", season)
+                        .queryParam("per_page", 100)
+                        .build())
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+    }
 }
